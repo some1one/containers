a53b958e73b6a3c6a6b888be23201de8320807af
Justin Phillips <justin9490@gmail.com>
2021-12-27 06:56:02 +0000

    base images created

---
+++ README.md
@@ -0,0 +1,3 @@
+Using debian as the OS since it seems the most compatible for build environments
+Generally using buster for backwards compatibility
+using the buildpack-deps images as the base since it includes the basic packages common to almost all dev containers
---
+++ base/meta.sh
@@ -0,0 +1,5 @@
+# Persist image metadata info, script if meta.env found in same directory
+if [ -f "${SCRIPT_DIR}/meta.env" ]; then
+    mkdir -p /usr/local/etc/vscode-dev-containers/
+    cp -f "${SCRIPT_DIR}/meta.env" /usr/local/etc/vscode-dev-containers/meta.env
+fi
